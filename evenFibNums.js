80249a485dffdf6966f1d70480987666ce7394d4
DIR\estefania.morton <estefania.morton@accenture.com>
2019-01-21 13:02:16 -0600

    ejercicios

---
+++ evenFibNums.js
@@ -0,0 +1,35 @@
+/**
+ * Return the total sum of all even fibonacci numbers up to and including
+ * the value given to you at `maxFibValue`
+ *
+ * @param  {Number} maxFibValue
+ * @return {Number} sum
+ */
+
+function _sumFibs( maxFibValue ) {
+  var sum = 0;
+
+  // do your work here
+  return sum;
+}
+
+// bonus round
+function _highestFibonacciNumber (maxFibValue){
+  var highest = 0;
+
+  //define your base case, validate your input
+
+
+  //do your work here
+
+  return highest;
+}
+
+/**
+ * Do not modify code below.
+ * You must be at least level 10 to understand.
+ */
+module.exports = {
+  sumFibs : _sumFibs,
+  highestFibonacciNumber : _highestFibonacciNumber
+};
